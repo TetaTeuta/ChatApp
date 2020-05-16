5c1061ddade73cb4c41b409e834333aec2c5f269
TetaTeuta <teuta.stazic@gmail.com>
2020-05-16 14:34:15 +0200

    5.5 working

--- components/Chat.js
+++ components/Chat.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
-import { GiftedChat, Bubble } from 'react-native-gifted-chat';
-import Platform from 'react-native';
-import { StyleSheet, ImageBackground, Text, TextInput, Alert, TouchableOpacity, Button, View } from 'react-native';
-import firebase from "firebase";
-import "firebase/firestore";
-
-export default class Chat extends React.Component {
-
+import { View, StyleSheet, Platform, AsyncStorage } from 'react-native';
+import KeyboardSpacer from 'react-native-keyboard-spacer';
+import { GiftedChat, Bubble, InputToolbar } from 'react-native-gifted-chat';
+import NetInfo from '@react-native-community/netinfo';
+import firebase from 'firebase';
+import 'firebase/firestore';
+export default class Chat extends Component {
   constructor() {
     super();
+    this.state = {
+      messages: [],
+      user: {
+        _id: '',
+        name: '',
+        avatar: '',
+      },
+      isConnected: false,
+    };
+    // Firebase init
     if (!firebase.apps.length) {
       firebase.initializeApp({
         apiKey: "AIzaSyDIVBDt0W3t0UVpotXQbhj7GeX4khPAgKk",
@@ -19,155 +28,175 @@ export default class Chat extends React.Component {
         messagingSenderId: "967149298342",
         appId: "1:967149298342:web:a59ab9e170511b9f564a67",
         measurementId: "G-2K4CQY79QF"
-      })
+      });
     }
-    this.referenceMessageUser = null;
-    this.referenceMessages = firebase.firestore().collection('messages')
-
-    this.state = {
-      messages: [],
-      uid: 0
-    };
+    this.referenceMessages = firebase.firestore().collection('messages');
   }
-
-  //this will put the users name in navigation bar
+  // Set navigation title as username
   static navigationOptions = ({ navigation }) => {
     return {
-      title: navigation.state.params.name,
+      title: navigation.state.params.name
     };
   };
-
+  // Display elements
   componentDidMount() {
-    // listen to authentication events
-    this.authUnsubscribe = firebase.auth().onAuthStateChanged(async user => {
-      if (!user) {
-        await firebase.auth().signInAnonymously();
-      }
-      if (this.props.navigation.state.params.name) {
-        this.setUser(user.uid, this.props.navigation.state.params.name);
+    NetInfo.fetch().then(state => {
+      //console.log('Connection type', state.type);
+      if (state.isConnected) {
+        //console.log('Is connected?', state.isConnected);
+        this.authUnsubscribe = firebase.auth().onAuthStateChanged(async user => {
+          if (!user) {
+            try {
+              await firebase.auth().signInAnonymously();
+            } catch (error) {
+              console.log('Unable to sign in: ' + error.message);
+            }
+          }
+          this.setState({
+            isConnected: true,
+            user: {
+              _id: user.uid,
+              name: this.props.navigation.state.params.name,
+              avatar: 'https://placeimg.com/140/140/any',
+            },
+            loggedInText: this.props.navigation.state.params.name + ' has entered the chat',
+            messages: [],
+          });
+          //console.log(user);
+          this.unsubscribe = this.referenceMessages.orderBy('createdAt', 'desc').onSnapshot(this.onCollectionUpdate);
+        });
       } else {
-        this.setUser(user.uid);
+        this.setState({
+          isConnected: false,
+        });
+        this.getMessages();
       }
-      this.setState({
-        uid: user.uid,
-        loggedInText: 'Welcome'
-      });
-
-      this.unsubscribe = this.referenceMessages.onSnapshot(this.onCollectionUpdate);
     });
-
-    this.setState({
-      messages: [
-        {
-          _id: 124098920,
-          text: 'Hi there',
-          createdAt: new Date(),
-          user: {
-            name: "Teuta",
-            _id: 5,
-            avatar: 'https://placeimg.com/140/140/any',
-          },
-        },
-        {
-          _id: 2,
-          text: this.props.navigation.state.params.name + ' is in da house!',
-          createdAt: new Date(),
-          system: true,
-        }
-      ]
-    })
-  }
-
+  };
   componentWillUnmount() {
-    // stop listening to authentication
+    // Stop listening for authentication
     this.authUnsubscribe();
+    // Stop listening for changes
     this.unsubscribe();
-  }
-
-  setUser = (_id, name = "Anonymous") => {
-    this.setState({
-      user: {
-        _id: _id,
-        name: name,
-        avatar: "https://placeimg.com/140/140/tech"
-      }
-    });
-  }
-
-  get user() {
-    return {
-      name: this.props.navigation.state.params.name,
-      _id: this.state.uid,
-      id: this.state.uid,
-    }
-  }
-
-  addMessage() {
-    console.log(this.state.messages[0].user)
-    this.referenceMessages.add({
-      _id: this.state.messages[0]._id,
-      text: this.state.messages[0].text || '',
-      createdAt: this.state.messages[0].createdAt,
-      user: this.state.messages[0].user,
-      uid: this.state.uid,
-    });
-  }
-
-  onSend(messages = []) {
-    this.setState(
-      previousState => ({
-        messages: GiftedChat.append(previousState.messages, messages)
-      }),
-      () => {
-        this.addMessage();
-      }
-    );
-  }
-
+  };
   onCollectionUpdate = (querySnapshot) => {
     const messages = [];
-    // go through each document
+    // Go through each document
     querySnapshot.forEach(doc => {
-      // get the QueryDocumentSnapshot's data
+      // Get queryDocumentSnapshot's data
       var data = doc.data();
       messages.push({
         _id: data._id,
-        text: data.text,
+        text: data.text || '',
         createdAt: data.createdAt.toDate(),
         user: data.user,
-        image: data.image,
       });
     });
     this.setState({
       messages
     });
   };
-
+  // Add message
+  addMessage() {
+    const message = this.state.messages[0];
+    this.referenceMessages.add({
+      _id: message._id,
+      text: message.text || '',
+      createdAt: message.createdAt,
+      user: this.state.user,
+    });
+  };
+  // Get messages from local(async) storage
+  async getMessages() {
+    let messages = [];
+    try {
+      messages = await AsyncStorage.getItem('messages') || [];
+      this.setState({
+        messages: JSON.parse(messages)
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  // Save messages locally(asyncStorage)
+  async saveMessages() {
+    try {
+      await AsyncStorage.setItem('messages', JSON.stringify(this.state.messages));
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  // Delete messages locally(asyncStorage)
+  async deleteMessages() {
+    try {
+      await AsyncStorage.removeItem('messages');
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  // Send message
+  onSend = (messages = []) => {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages)
+    }), () => {
+      this.addMessage();
+      this.saveMessages();
+    });
+  };
+  // Hide inputbar when offline
+  renderInputToolbar(props) {
+    if (this.state.isConnected) {
+      return (
+        <InputToolbar
+          {...props}
+        />
+      );
+    }
+  };
+  // Change message bubble color
   renderBubble(props) {
     return (
       <Bubble
         {...props}
         wrapperStyle={{
           right: {
-            backgroundColor: '#000'
+            backgroundColor: '#0E3B43'
+          },
+          left: {
+            backgroundColor: '#357266'
           }
         }}
       />
-    )
-  }
-
+    );
+  };
   render() {
     return (
-      <View style={[{ flex: 1 }, { backgroundColor: this.props.navigation.state.params.color }]}>
-        <Text>{this.state.loggedInText}</Text>
+      <View style={[
+        styles.container,
+        { backgroundColor: this.props.navigation.state.params.color },
+      ]}>
         <GiftedChat
-          renderBubble={this.renderBubble}
-          messages={this.state.messages}
-          onSend={messages => this.onSend(messages)}
+          scrollToBottom
+          showUserAvatar={true}
           user={this.state.user}
+          messages={this.state.messages}
+          renderUsernameOnMessage={true}
+          showAvatarForEveryMessage={true}
+          renderActions={this.renderCustomActions}
+          onSend={messages => this.onSend(messages)}
+          renderBubble={this.renderBubble.bind(this)}
+          renderInputToolbar={this.renderInputToolbar.bind(this)}
+          timeTextStyle={{ left: { color: '#F5F5F5' }, right: { color: '#F5F5F5' } }}
         />
-        {Platform.OS === 'android' ? <KeyboardSpacer /> : null}
+        {/* {Platform.OS === 'android' ? <KeyboardSpacer /> : null} */}
       </View>
-    )
+    );
   }
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    color: '#FFFFFF',
+    backgroundColor: '#000000',
+  }
+});
